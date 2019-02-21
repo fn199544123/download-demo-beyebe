b1f644658ebbf247e926b22a2f0a77d3b0153e53
fangnan <gudandezhupi@163.com>
2019-02-21 14:09:42 +0800

    scrapy-demo-beyebe v2.21 修改错误端口9189至9089

--- webdriver_service/django_start/manage_zhongdeng.py
+++ webdriver_service/django_start/manage_zhongdeng.py
@@ -31,6 +31,6 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    args = [sys.argv[0], "runserver", "0.0.0.0:9189", "--noreload"]
+    args = [sys.argv[0], "runserver", "0.0.0.0:9089", "--noreload"]
     print(args)
     execute_from_command_line(args)
