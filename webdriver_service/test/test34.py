ad6ac47020269d440f8db99677376c3c0e9711ca
fangnan <gudandezhupi@163.com>
2019-02-28 14:24:05 +0800

    scrapy-demo-beyebe v2.52 name改为pdfname，与刘琛适配

--- webdriver_service/test/test34.py
+++ webdriver_service/test/test34.py
@@ -8,5 +8,15 @@ url = "http://39.108.188.34:9089/spider/zhongdeng.go?companyName={}".format(comp
 text = requests.get(url).text
 objJson = json.loads(text)
 print(objJson)
+print("PDF数量", len(objJson['ansList']))
 
+url = "http://39.108.188.34:8891/BatchPdfCode"
+data = {
+    "pdfs": objJson['ansList'],
+    "billNums": ["01287153", "01238774"],
+}
 
+data = json.dumps(data)
+headers = {"Content-Type": "application/json"}
+response = requests.post(url, data=data, headers=headers)
+print(response.text)
