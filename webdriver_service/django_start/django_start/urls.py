42628dbcfbae061dc9401db5537e5748dff2d6a0
fangnan <gudandezhupi@163.com>
2019-03-13 11:14:35 +0800

    scrapy-demo-beyebe v3.23 错误码修改

--- webdriver_service/django_start/django_start/urls.py
+++ webdriver_service/django_start/django_start/urls.py
@@ -13,19 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from rest_framework.schemas import get_schema_view
-
 from webdriver_service.django_start.django_start.view import changeModel, changeModelBatch
 from webdriver_service.django_start.manage import hello
-from django.conf.urls import url, include
-from django.contrib import admin
-from rest_framework import routers
-
-# 视图
-from rest_framework import viewsets
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from django.conf.urls import url
 
 urlpatterns = [
     url(r"test.go", hello),
